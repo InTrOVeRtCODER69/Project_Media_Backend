2fac93bbaef8018c92671a9dfee630ca087f7506
SAD LIFE <indeedintrovert@gmail.com>
2025-06-21 23:33:47 +0530

    Initial set up day -1

---
+++ src/main/java/com/Chatbuzz/Chatbuzz/ChatbuzzApplication.java
@@ -0,0 +1,13 @@
+package com.Chatbuzz.Chatbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbuzzApplication.class, args);
+	}
+
+}
